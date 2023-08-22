b81f955ef1087fe6a044838b5608c9ae9aeb9b02
Aubrey Lizardo <116300259+Br3y@users.noreply.github.com>
2023-08-22 08:50:31 +0800

    moving capitalize.js to src folder and change extension to html

--- capitalize.js
+++
@@ -1,7 +0,0 @@
-function capitalize(string){
-    // get the first letter and upper case it then slice the remaining letters
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-const getString = prompt("enter string");
-alert(capitalize(getString))
