252d7afe40fdc5524bb05ca6d4fc9fe6900c5954
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-12-21 15:50:40 +0800

    value after deleting and returning array

--- rubyExercises/array.rb
+++ rubyExercises/array.rb
@@ -6,3 +6,18 @@ number = 3
 puts arr.include?(number) ? "array is here" : "not"
 
 puts "----------------------------"
+
+# What will the following programs return? What is the value of arr after each?
+
+arr = ["b", "a"]
+arr = arr.product(Array(1..3))
+arr.first.delete(arr.first.last)
+puts arr.to_s
+
+arr = ["b", "a"]
+arr2 = arr.product([Array(1..3)])
+arr2.first.delete(arr2.first.last)
+puts arr2.to_s
+
+puts "----------------------------"
+
