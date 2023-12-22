5c008b1d4d8cc9657f74e0dc3a8d4f1a990ecbff
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-12-22 19:30:09 +0800

    each_with_index own solution to exercise

--- rubyExercises/array.rb
+++ rubyExercises/array.rb
@@ -66,3 +66,18 @@ top_five_games = ["mario brothers","excite bike","ring king","castlevania","doub
 top_five_games.each_with_index do |val, idx|
     puts "#{idx}. #{val}"
 end
+
+array = [1, 2, 3, 4, 5]
+new_array = []
+array.each_with_index do |value, index|
+    puts "#{index}. #{value}"
+end
+
+
+puts "----------------------------"
+=begin
+    Write a program that iterates over an array and builds a new array that is the result of
+    incrementing each value in the original array by a value of 2. You should have two
+    arrays at the end of this program, The original array and the new array you've created.
+    Print both arrays to the screen using the p method instead of puts.
+=end
