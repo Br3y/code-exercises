594a039b5d7f0ed274b4fa1fd2867eeed2626cbc
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-12-26 13:39:25 +0800

    Print odd from 3 to 20

--- hackerhero/learn-to-code/main.js
+++ hackerhero/learn-to-code/main.js
@@ -8,3 +8,17 @@ function print1to20() {
 }
 
 console.log(print1to20())
+
+
+console.log("----------------------------------------------------------")
+
+
+console.log("Create a function that prints/logs all the odd numbers from 3 to 20.");
+function printOdd3to20() {
+  let result = ""
+  for (let i = 3; i < 20; i += 2) {
+    result = result + " " + i
+  }
+  return result
+}
+console.log(printOdd3to20())
