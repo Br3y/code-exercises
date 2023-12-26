c2a557cea666065d2712f23b0ada3901d5a3e30d
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-12-26 17:42:42 +0800

    no negatives in arr ( make it 0 )

--- hackerhero/learn-to-code/main.js
+++ hackerhero/learn-to-code/main.js
@@ -136,3 +136,18 @@ function squareVal(arr){
   return arr
 }
 console.log(squareVal(arr))
+
+
+console.log("----------------------------------------------------------")
+console.log("Given an array with multiple values, write a function that replaces any negative numbers within the array with the value of 0.")
+console.log("When the program is done the array should contain no negative values. (e.g. [1,5,10,-2] will become [1,5,10,0])")
+function noNeg(arr){
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] < 0){
+        arr[i] = 0
+    }    
+  }
+  return arr
+}
+arr = [1, 5, 10, -2]
+console.log(noNeg(arr))
