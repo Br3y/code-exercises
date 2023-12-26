b042c2e5adf1cd85dc4cedc3bb68e18d0c79be18
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-12-26 15:58:45 +0800

    be positive

--- hackerhero/learn-to-code/main.js
+++ hackerhero/learn-to-code/main.js
@@ -109,3 +109,18 @@ function printPositives(arr){
 }
 arr = [-1, 3, -5, 10]
 console.log(printPositives(arr))
+
+
+console.log("----------------------------------------------------------")
+console.log("Given an array, write a function that returns a new array where each negative value was converted to a positive value.")
+console.log("For example, bePositive([-1,-3,-5]) returns [1, 3, 5]. A positive number in the original array should remain as positive, so that each number in the new array is all positive.")
+function bePositive(arr){
+  let posArr = []
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] < 0){
+        arr[i] *= -1
+    }
+  }
+  return arr
+}
+console.log(bePositive(arr))
