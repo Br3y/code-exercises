6f0788fa19254e2c62a02fbe44161c9f443c50c2
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-12-28 18:13:59 +0800

    swap values (first and last index)

--- hackerhero/learn-to-code/main.js
+++ hackerhero/learn-to-code/main.js
@@ -304,3 +304,14 @@ function greaterThanSecond(arr){
 }
 console.log(greaterThanSecond(arr))
 
+
+console.log("----------------------------------------------------------")
+console.log("Write a function that will swap the first and last values in any given array. The default minimum length of the array is 2.")
+console.log("For example, swapFirstLast([1,3,5]) should return [5, 3, 1]. swapFirstLast([1,3,5,7,9]) should return [9, 3, 5, 7, 1].")
+function swapFirstLast(arr){
+  let firstArr = arr[0]
+  arr[0] = arr[arr.length - 1]
+  arr[arr.length -1] = firstArr
+  return arr
+}
+console.log(swapFirstLast(arr))
