acee47377f6707c057bde326f3e70102270368cf
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-12-29 20:54:16 +0800

    debugging error, multiple question with one answer

--- rubyExercises/hash.rb
+++ rubyExercises/hash.rb
@@ -85,3 +85,13 @@ my_hash2 = {x => "some value"}
 puts my_hash
 puts my_hash2
 puts "first one is symbol x and the second one is string variable"
+
+
+puts "----------------------------"
+# If you see this error, what do you suspect is the most likely problem?
+puts "NoMethodError: undefined method `keys' for Array"
+puts "A. We're missing keys in an array variable."
+puts "B. There is no method called keys for Array objects."
+puts "C. keys is an Array object, but it hasn't been defined yet."
+puts "D. There's an array of strings, and we're trying to get the string keys out of the array, but it doesn't exist."
+puts "B is the answer"
