70acd94f1d6ed5908afb64eb48a115b372b7d261
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-12-29 18:06:32 +0800

    difference between merge and merge!

--- rubyExercises/hash.rb
+++ rubyExercises/hash.rb
@@ -15,3 +15,20 @@ puts "#{siblings}"
 # end
 # arr = immediate_family.values.flatten
 # p arr
+
+
+puts "----------------------------"
+# Look at Ruby's merge method. Notice that it has two versions. What is the difference between merge and merge!? Write a program that uses both and illustrate the differences.
+# My own Solution
+puts "'merge' is indestructable while 'merge!' is destructable which affects the original value"
+
+# Solution
+puts "The difference is merge! modifies permanently, while merge does not."
+cat = {name: "whiskers"}
+weight = {weight: "10 lbs"}
+puts cat.merge(weight)
+puts cat                  # => {:name=>"whiskers"}
+puts weight               # => {:weight=>"10 lbs"}
+puts cat.merge!(weight)
+puts cat                  # => {:name=>"whiskers", :weight=>"10 lbs"}
+puts weight               # => {:weight=>"10 lbs"}
