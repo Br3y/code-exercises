380fecd8233467eda93f7b39c43f8718177eef54
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-12-29 19:38:10 +0800

    print key, value and both

--- rubyExercises/hash.rb
+++ rubyExercises/hash.rb
@@ -32,3 +32,30 @@ puts weight               # => {:weight=>"10 lbs"}
 puts cat.merge!(weight)
 puts cat                  # => {:name=>"whiskers", :weight=>"10 lbs"}
 puts weight               # => {:weight=>"10 lbs"}
+
+
+puts "----------------------------"
+# Using some of Ruby's built-in Hash methods, write a program that loops through a hash and prints all of the keys. Then write a program that does the same thing except printing the values. Finally, write a program that prints both.
+# My Own Solution
+grades = {
+    math: 96,
+    science: 90,
+    english: 92,
+    programming: 99,
+    oop: 96
+}
+# grades.each do |key, value|
+#     puts "#{key}"
+# end
+# grades.each do |key, value|
+#     puts "#{value}"
+# end
+grades.each do |key, value|
+    puts "#{key} = #{value}"
+end
+
+# Solution
+# opposites = {positive: "negative", up: "down", right: "left"}
+# opposites.each_key { |key| puts key }
+# opposites.each_value { |value| puts value }
+# opposites.each { |key, value| puts "The opposite of #{key} is #{value}" }
