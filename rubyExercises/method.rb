dd0015606986be7d33b210bfef9f5a5f65bd28f0
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-01-03 14:11:51 +0800

    Revert "predict each expressions evaluate to"
    This reverts commit 3e2da9eac8914c53012b4ea1f47c1715f179deee.

--- rubyExercises/method.rb
+++ rubyExercises/method.rb
@@ -2,13 +2,6 @@
 def greeting(name)
     return "Hello #{name}"
 end
+
 puts greeting("Aubrey")
 
-
-puts "----------------------------"
-# What do the following expressions evaluate to? That is, what value does each expression return?
-x = 2 # =>
-puts x = 2 # => nil
-p name = "Joe" # => "Joe"
-four = "four" # => "four"
-print something = "nothing" # => nil
