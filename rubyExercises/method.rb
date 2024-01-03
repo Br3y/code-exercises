e0fc83c80af5ca1e8a935d7011538c6683fc8aab
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-01-03 14:42:02 +0800

    return Yippeee!!!!

--- rubyExercises/method.rb
+++ rubyExercises/method.rb
@@ -21,5 +21,13 @@ def scream(words)
     return
     puts words
 end
-  
 scream("Yippeee")
+
+
+puts "----------------------------"
+# 1) Edit the method definition in exercise #4 so that it does print words on the screen. 2) What does it return now?
+def scream(words)
+    words = words + "!!!!"
+    return words
+end
+puts scream("Yippeee")
