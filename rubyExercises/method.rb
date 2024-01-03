854037e6e5f34d8b1825118fd8f2c2193c436141
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-01-03 14:47:19 +0800

    understand what error means

--- rubyExercises/method.rb
+++ rubyExercises/method.rb
@@ -31,3 +31,14 @@ def scream(words)
     return words
 end
 puts scream("Yippeee")
+
+
+puts "----------------------------"
+# What does the following error message tell you?
+=begin
+ArgumentError: wrong number of arguments (1 for 2)
+  from (irb):1:in `calculate_product'
+  from (irb):4
+  from /Users/username/.rvm/rubies/ruby-2.5.3/bin/irb:12:in `<main>'
+=end
+puts "wrong number of arguments (2 arguments need but only provide 1)"
