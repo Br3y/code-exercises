bee95f2a912356b864e2c46cbc2e8b217e2e3b22
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-01-05 15:49:20 +0800

    answer to what will return

--- rubyExercises/more_stuff_launchschool.rb
+++ rubyExercises/more_stuff_launchschool.rb
@@ -17,5 +17,6 @@ puts "What will the following program print to the screen? What will it return?"
 def execute(&block)
     block
 end
-  
 execute { puts "Hello from inside the execute method!" }
+puts "Answer: nothing will return"
+puts "Nothing is printed to the screen because the block is never activated with the .call method. The method returns a Proc object."
