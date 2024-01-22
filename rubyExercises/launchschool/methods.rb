e0f863e45404f20c66c4289a25a548822740b6e1
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-01-22 23:16:39 +0800

    launchschool: methods exercises

---
+++ rubyExercises/launchschool/methods.rb
@@ -0,0 +1,16 @@
+
+puts "1. Write a program that prints a greeting message. This program should contain a method called greeting that takes a name as its parameter and returns a string."
+def greeting(name)
+    "Hi #{name}"
+end
+puts greeting("Aubrey")
+puts "-------------------------------------------------------"
+
+puts "3. Write a program that includes a method called multiply that takes two arguments and returns the product of the two numbers."
+def multiply(num1, num2)
+    "num1: #{num1} multiple to num2: #{num2} is equal to #{num1 * num2}"
+end
+puts multiply(3, 3)
+puts "-------------------------------------------------------"
+
+puts "Other problems are solved by reading the question and make you understanding what will the output will be."
