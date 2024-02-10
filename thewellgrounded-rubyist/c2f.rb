cc458697157b8bb35612720a3fb1422d3417242d
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-02-10 16:25:48 +0800

    fix: change puts to print for better readability

--- thewellgrounded-rubyist/c2f.rb
+++ thewellgrounded-rubyist/c2f.rb
@@ -1,9 +1,8 @@
 celsius = 100
 fahrenheit = (celsius * 9 / 5) + 32
-puts "The result is: "
-puts fahrenheit
+print "The result is: "
+print fahrenheit
 puts "."
-
 
 # ruby -cw c2f.rb
 # -c --> check for syntax error
