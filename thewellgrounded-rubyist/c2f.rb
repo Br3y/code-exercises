a8b9a68459a14011356620b9a88abccc69dd74f3
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-02-10 16:25:13 +0800

    feat: add interesting debug -cw

--- thewellgrounded-rubyist/c2f.rb
+++ thewellgrounded-rubyist/c2f.rb
@@ -3,3 +3,8 @@ fahrenheit = (celsius * 9 / 5) + 32
 puts "The result is: "
 puts fahrenheit
 puts "."
+
+
+# ruby -cw c2f.rb
+# -c --> check for syntax error
+# -w --> activate a higher level of warning
