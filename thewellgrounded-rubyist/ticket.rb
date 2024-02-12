db23bfcb02868907da34968b16b017536fe5a8b7
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-02-12 13:49:45 +0800

    build: def methods for concert using ticket

---
+++ thewellgrounded-rubyist/ticket.rb
@@ -0,0 +1,24 @@
+def ticket_date
+  "01/02/03"
+end
+
+def ticket_venue
+  "Town Hall"
+end
+
+def ticket_event
+  "Author's reading"
+end
+
+def ticket_performer
+  "Mark Twain"
+end
+
+def ticket_seat
+  "Second Balcony, row J, seat 12"
+end
+
+def ticket_price
+  5.50
+end
+
