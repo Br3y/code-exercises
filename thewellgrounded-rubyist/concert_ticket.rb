5fa28d975eb68e4dd5cfcf9b2a784b15413edeef
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-02-14 04:33:07 +0800

    fix: line space

--- thewellgrounded-rubyist/concert_ticket.rb
+++ thewellgrounded-rubyist/concert_ticket.rb
@@ -1,15 +1,19 @@
 class Ticket
+
   def initialize(venue, date, price)
     @venue = venue
     @date = date
     @price = price
   end
+  
   def venue
     @venue
   end
+  
   def date 
     @date
   end
+
   def price
     @price
   end
