7e83ed543d633f4210582a0d27c2e203d71bdcb4
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-02-14 05:37:41 +0800

    fix: change def get_methods to attr_reader to remove repetiveness

--- thewellgrounded-rubyist/concert_ticket.rb
+++ thewellgrounded-rubyist/concert_ticket.rb
@@ -1,21 +1,10 @@
 class Ticket
+  attr_reader :venue, :date, :price
 
   def initialize(venue, date, price)
     @venue = venue
     @date = date
     @price = price
-  end
-  
-  def venue
-    @venue
-  end
-  
-  def date 
-    @date
-  end
-
-  def price
-    @price
   end
 
   def price=(amount)
