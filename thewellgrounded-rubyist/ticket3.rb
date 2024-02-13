d4e585c93a70aee4f07371f293f381227334da96
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-02-14 03:23:29 +0800

    learn: overriding method, last def method create will be called

--- thewellgrounded-rubyist/ticket3.rb
+++ thewellgrounded-rubyist/ticket3.rb
@@ -6,3 +6,15 @@ end
 
 ticket = Ticket.new
 puts ticket.event
+
+class C
+  def m
+    puts "First definition of method m"
+  end
+
+  def m
+    puts "Second definition of method m"
+  end
+end 
+
+C.new.m
