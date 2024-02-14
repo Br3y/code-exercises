a5decd5efaa3608fe3099c88a93eaf29239bfef8
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-02-14 13:00:28 +0800

    fix: :price will be include inside attr_accessor and remove from others

--- thewellgrounded-rubyist/concert_ticket.rb
+++ thewellgrounded-rubyist/concert_ticket.rb
@@ -1,6 +1,7 @@
 class Ticket
-  attr_reader :venue, :date, :price
-
+  attr_reader :venue, :date
+  attr_accessor :price
+  
   def initialize(venue, date, price)
     @venue = venue
     @date = date
