0005823a47c790e493f512da489b2aca78e3417a
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-02-15 01:48:27 +0800

    feat: check who's the most expensive venue based on price

--- thewellgrounded-rubyist/concert_ticket.rb
+++ thewellgrounded-rubyist/concert_ticket.rb
@@ -11,9 +11,14 @@ class Ticket
   def price=(amount)
     @price = amount
   end
+
+  def Ticket.most_expensive(*tickets)
+    tickets.max_by(&:price)
+  end
 end
 
-th = Ticket.new("Town Hall", "11/12/13", 63.00)
-puts th.price
-th.price = 62.00
-puts th.price
+th = Ticket.new("Town Hall", "11/12/13", 12.55)
+cc = Ticket.new("Convention Center", "12/13/14", 10.00)
+fg = Ticket.new("Fairgrounds", "13/14/15", 18.00)
+highest = Ticket.most_expensive(th, cc, fg)
+puts "The highest-priced ticket is the one for #{highest.venue}."
